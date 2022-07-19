3e6c19db154588d520974f739811d5fb9f6eef9b
Amir-2018 <101711046+Amir-2018@users.noreply.github.com>
2022-07-19 17:00:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const csvdb = require('csv-database');
 const express = require('express');
 const app = express()
-const port = 3000
+const port = process.env.PORT;
 var sqlite3 = require('sqlite3');
 var bodyParser = require('body-parser')
 app.use(express.json());
